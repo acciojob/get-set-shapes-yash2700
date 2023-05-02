587450b98ba364cbcf1ebdad3fa067485c0f6961
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-02 17:14:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width=width;
+		this.height=height;
+	}
+	get getWidth(){
+		return this.width;
+	}
+	get getHeight(){
+		return this.height;
+	}
+	getArea(){
+		return this.width*this.height;
+	}
+	
+}
 
-class Square extends Animal {}
-
+class Square extends Rectangle {
+	 constructor(side){
+      super(side, side);
+      this.side = side;
+      }
+	getPerimeter(){
+		return 4*this.side;
+	}
+}
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
